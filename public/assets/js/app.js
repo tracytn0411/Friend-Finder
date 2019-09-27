dd1a0f83baac502751fd3b1e0c512b45639b1d33
TracyTN <tracytn0411@gmail.com>
2019-09-26 21:25:17 -0700

    Add radio buttons to survey questions.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,7 @@
 //---------- CLIENT-SIDE Javascript ------//
 
 //Function to display /friends.json in home page (index.html)
-  //For testing purpose : check if sever is working
+//For testing purpose : check if sever is working
 function getFriends() {
   $('#testing').empty();
 
@@ -10,8 +10,8 @@ function getFriends() {
     method: 'GET'
   }).then(function (friends) {
     for (var friendIndex in friends) {
-      console.log(friendIndex);
-      console.log(friends[friendIndex]);
+      //console.log(friendIndex);
+      //console.log(friends[friendIndex]);
       var p = $('<p>');
       p.text(`id: ${friends[friendIndex].id}, friend name: ${friends[friendIndex].friend_name}, link: ${friends[friendIndex].picture_link}`);
 
@@ -21,23 +21,76 @@ function getFriends() {
 }
 getFriends();
 
-//Function to pass user input to database
-  // button class (.btn) on.click won't work here. Need to use form id (#)
-// $("#insert_user").submit(function (event) {
-//   event.preventDefault(); // avoid to execute the actual submit of the form
-//   var un = $('#insert_user input[name = "friend_name"]').val();
-//   var up = $('#insert_user input[name = "picture_link"]').val();
-//   console.log(un);
-//   console.log(up);
 
-//   $.ajax({
-//     url: '/user-input',
-//     method: 'GET',
-//     data: {
-//       friend_name: un,
-//       picture_link: up,
-//     }
-//   }).then(function (message) {
-//     getFriends();
-//   })
-// })
+//Function to attach radio buttons to each question and display on survey.html
+getQuestions = () => {
+  $.ajax({
+      url: '/survey-questions',
+      method: 'GET'
+    })
+    .then(function (questions) {
+      for (let questionIndex in questions) {
+        console.log(questionIndex);
+        console.log(questions[questionIndex]);
+
+        let surveySection = $('<fieldset>').addClass('form-group')
+        let sQuestion = $('<h3>').text(`Question ${questions[questionIndex].id}. ${questions[questionIndex].question}`);
+        let sRadio1 = $('<section>').addClass('form-check form-check-inline').append([
+          $('<label>').addClass('form-check-label').attr('for', 'sRadio1').text('1'),
+          $('<input>').addClass('form-check-input').attr({
+            'type': 'radio',
+            'value': 'op1',
+            'id': 'sRadio1',
+            'name': `question${questions[questionIndex].id}`
+          })
+        ])
+        let sRadio2 = $('<section>').addClass('form-check form-check-inline').append([
+          $('<label>').addClass('form-check-label').attr('for', 'sRadio2').text('2'),
+          $('<input>').addClass('form-check-input').attr({
+            'type': 'radio',
+            'value': 'op2',
+            'id': 'sRadio2',
+            'name': `question${questions[questionIndex].id}`
+          })
+        ])
+        let sRadio3 = $('<section>').addClass('form-check form-check-inline').append([
+          $('<label>').addClass('form-check-label').attr('for', 'sRadio3').text('3'),
+          $('<input>').addClass('form-check-input').attr({
+            'type': 'radio',
+            'value': 'op3',
+            'id': 'sRadio3',
+            'name': `question${questions[questionIndex].id}`
+          })
+        ])
+        let sRadio4 = $('<section>').addClass('form-check form-check-inline').append([
+          $('<label>').addClass('form-check-label').attr('for', 'sRadio4').text('4'),
+          $('<input>').addClass('form-check-input').attr({
+            'type': 'radio',
+            'value': 'op4',
+            'id': 'sRadio4',
+            'name': `question${questions[questionIndex].id}`
+          })
+        ])
+        let sRadio5 = $('<section>').addClass('form-check form-check-inline').append([
+          $('<label>').addClass('form-check-label').attr('for', 'sRadio5').text('5'),
+          $('<input>').addClass('form-check-input').attr({
+            'type': 'radio',
+            'value': 'op5',
+            'id': 'sRadio5',
+            'name': `question${questions[questionIndex].id}`
+          })
+        ])
+
+        $('<div>').addClass('form-check')
+        surveySection.append(sQuestion)
+        surveySection.append([sRadio1, sRadio2, sRadio3, sRadio4, sRadio5])
+        $('#insert_user').append(surveySection);
+      }
+
+      let surveyBtn = $('<div>').addClass('form-group').append(
+        $('<button>').addClass('btn btn-info').text('Submit')
+      )
+      $('#insert_user').append(surveyBtn)
+    })
+}
+getQuestions();
